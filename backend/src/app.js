ebf3d2b4487e10a76db7159f38baf0344c6e0d75
Gautam Kumar <gautamkumark001@gmail.com>
2025-05-07 23:38:07 +0530

    Deploying Froentend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,39 +1,35 @@
-import express from "express";
-import { createServer } from "node:http";
+import express from 'express';
+import { createServer } from 'node:http';
 
-import { Server } from "socket.io";
+import { Server } from 'socket.io';
 
-import mongoose from "mongoose";
-import { connectToSocket } from "./controllers/socketManager.js";
+import mongoose from 'mongoose';
+import { connectToSocket } from './controllers/socketManager.js';
 
-import cors from "cors";
-import userRoutes from "./routes/users.routes.js";
+import cors from 'cors';
+import userRoutes from './routes/users.routes.js';
 
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
-
-app.set("port", (process.env.PORT || 8000))
+app.set('port', process.env.PORT || 8000);
 app.use(cors());
-app.use(express.json({ limit: "40kb" }));
-app.use(express.urlencoded({ limit: "40kb", extended: true }));
+app.use(express.json({ limit: '40kb' }));
+app.use(express.urlencoded({ limit: '40kb', extended: true }));
 
-app.use("/api/v1/users", userRoutes);
+app.use('/api/v1/users', userRoutes);
 
 const start = async () => {
-    app.set("mongo_user")
-    const connectionDb = await mongoose.connect("mongodb+srv://imdigitalashish:imdigitalashish@cluster0.cujabk4.mongodb.net/")
+    app.set('mongo_user');
+    const connectionDb = await mongoose.connect(
+        'mongodb+srv://gautamkumark001:QOfL3YhdT4ngNTve@cluster1.we1msm1.mongodb.net/'
+    );
 
-    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`)
-    server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 8000")
+    console.log(`MONGO Connected DB HOst: ${connectionDb.connection.host}`);
+    server.listen(app.get('port'), () => {
+        console.log('LISTENIN ON PORT 8000');
     });
-
-
-
-}
-
-
+};
 
 start();
